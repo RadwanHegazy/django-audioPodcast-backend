53702d67256159c06d381a5d2c558388d3745ed8
RadwanHegazy <radwangaber22@gmail.com>
2024-10-22 07:09:16 +0300

    write logic in consumers file

--- stage/ws/consumers.py
+++ stage/ws/consumers.py
@@ -1,5 +1,7 @@
 from channels.generic.websocket import WebsocketConsumer
 from ..models import Stage
+import json
+from asgiref.sync import async_to_sync
 
 class StageConsumer(WebsocketConsumer) : 
 
@@ -8,15 +10,46 @@ class StageConsumer(WebsocketConsumer) :
         
         try : 
             self.stage = Stage.objects.get(id=stage_id)
-            self.accept()
         except Stage.DoesNotExist:
             self.close()
             return
+        
+        self.GROUP = f'group_{stage_id}'
+        self.accept()
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.GROUP,
+            self.channel_name
+        )
+
 
     def disconnect(self, code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+            self.GROUP,
+            self.channel_name
+        )
 
     def receive(self, text_data=None, bytes_data=None):
-        print("recived : ", text_data)
+        json_data = json.loads(text_data)
+        action = json_data.get('action', None)
 
-    
+        if action == 'increase':
+            total_counter = self.stage.visitor_counter + 1
+            self.stage.visitor_counter = total_counter
+            self.stage.save()
+            async_to_sync(self.channel_layer.group_send)(
+                self.GROUP,{
+                    'type' : 'action',
+                    'data' : {
+                        'visitors' : total_counter
+                    }
+                }
+            )
+
+        elif action == 'close':
+            self.stage.delete()
+            self.close()
+            return
+
+    def action(self, data):
+        self.send(text_data=json.dumps(data['data']))
